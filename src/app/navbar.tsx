1097df8e8d8a51088d389a0af89e011ae0966ac2
Greg Patini <ninopatini@icloud.com>
2026-01-22 17:28:39 +0100

    add responsiveness to the landing page. Highly styled the navbar ul part and applied first Framer Motion

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -1,17 +1,49 @@
+"use client"
 import React from 'react';
+import { ChevronUpIcon } from '@heroicons/react/24/solid';
+import {motion} from 'framer-motion';
 
 const Navbar = () => {
   return (
-    <nav className="flex items-center justify-between p-4 fixed top-0 left-0 right-0 z-1">
+    <nav className="flex items-center justify-between p-4 fixed top-0 left-0 right-0 z-50">
       <a href="/" className="text-2xl font-bold">
-        <img src="/sign.svg" alt="Logo" className="h-8 w-auto" />
+        <img src="/sign.svg" alt="Logo" className="h-8 w-full" />
       </a>
-      <div className=" ">
-        <ul className="mr-4 flex items-center gap-x-14">
-          <li className="text-[10px] font-light tracking-[1px]">RISORSE</li>
-          <li className="text-[10px] font-light tracking-[1px]">CONTATTI</li>
-          <li className="rounded-tl-lg bg-[#2B3A54] px-4 py-2 text-[10px] font-light tracking-[1px] text-white">
-            CHI SONO
+      <div className="">
+        <ul className="mr-4 flex items-center gap-x-14 justify-center">
+          <motion.li 
+            className="text-[10px] font-light tracking-[1px] flex items-center"
+            whileHover="hover"
+            initial="initial"
+            variants={{
+              initial: {},
+              hover: {
+                scale: 1.05,
+                transition: { duration: 0.5 }
+              }
+            }}
+          >
+            <a href="/risorse" className="hover:text-[#2B3A54] flex items-center">
+              RISORSE
+              <motion.span
+                className="ml-2 inline-block"
+                variants={{
+                  initial: { rotate: 360 },
+                  hover: { rotate: 180 },
+                }}
+                transition={{ duration: 0.2 }}
+              >
+                <ChevronUpIcon className="w-3" />
+              </motion.span>
+            </a>
+          </motion.li>
+          <li className="text-[10px] font-light tracking-[1px]"><a href="/contatti" className="hover:underline">CONTATTI</a></li>
+          <li className="text-[10px] font-light tracking-[1px]">
+            <a href="/chi-sono" className="relative px-4 py-2 font-light text-white rounded-tl-lg group block">
+              <span className="absolute inset-0 w-full h-full transition duration-300 transform -translate-x-1 -translate-y-1 bg-[#2B3A54] ease opacity-80 group-hover:translate-x-0 group-hover:translate-y-0 rounded-tl-lg"></span>
+              <span className="absolute inset-0 w-full h-full transition duration-300 transform translate-x-1 translate-y-1 bg-[#1a2538] ease opacity-80 group-hover:translate-x-0 group-hover:translate-y-0 mix-blend-screen rounded-tl-lg"></span>
+              <span className="relative">CHI SONO</span>
+            </a>
           </li>
         </ul>
       </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,29 @@
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import Navbar from './navbar';
-import AnimatedHero from '@/components/animated-hero';
+import {motion} from 'framer-motion';
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-gradient-to-b from-[#e5e9f5] from-10% via-[#c6d6f5] via-50% to-[#b4d6d8] to-90% px-4">
+    <main className="min-h-screen bg-gradient-to-b from-[#e5e9f5] from-10% via-[#c6d6f5] via-50% to-[#b4d6d8] to-90%">
       <Navbar />
-      <div className="flex absolute ">
-        <div className="flex flex-col justify-center relative top-50 left-20">
-        <h1 className="text-5xl font-family-open-sans font-medium text-[#2B3A54]">Dott. Federico Benni</h1>
-          <p className="text-sm text-gray-500 font-family-roboto-mono font-light tracking-[2px] mt-10">Fisioterapista</p>
-          <p className="text-sm text-gray-500 font-family-roboto-mono font-light tracking-[2px] mt-[0.2px]">Master Universitario in Osteopatia</p>
-          </div>
-          </div>
-      <img src="/benni.jpg" alt="Foto di Federico Benni nella sua clinica" className="h-full absolute top-0 right-0  overflow-hidden" />
+      <div className="flex flex-col lg:flex-row lg:items-start lg:justify-between lg:gap-12 lg:pt-32 items-center justify-center pt-20 gap-8 ">
+        <div className="flex flex-col justify-center lg:pl-20 lg:mt-30">
+          <h1 className="text-3xl sm:text-4xl lg:text-5xl font-family-open-sans font-medium tracking-[2px] text-[#2B3A54]">
+            Dott. Federico Benni
+          </h1>
+          <p className="text-sm text-gray-500 font-family-roboto-mono font-light tracking-[2px] mt-10">
+            Fisioterapista
+          </p>
+          <p className="text-sm text-gray-500 font-family-roboto-mono font-light tracking-[2px] mt-[0.2px]">
+            Master Universitario in Osteopatia
+          </p>
+        </div>
+        <img 
+          src="/benni.jpg" 
+          alt="Foto di Federico Benni nella sua clinica" 
+          className=" w-full max-w-md mb-8 lg:absolute lg:top-0 lg:right-0 lg:h-full lg:w-auto lg:max-w-2xl lg:object-cover lg:z-0" 
+        />
+      </div>
     </main>
   );
 }
